9467545d85dc6e5d8ba2736140d94cd16677ef41
Hounaida202 <etrichih@gmail.com>
2025-02-09 14:47:11 +0000

    ajouter les routes vers les categories et les cours et la page sign up

--- index.php
+++ index.php
@@ -8,5 +8,6 @@ $router = new Router();
 $router->add('GET', '/youdemy-V2/', 'App\Controllers\HomeController', 'index');
 $router->add('GET', '/youdemy-V2/categories', 'App\Controllers\CategorieController', 'index');
 $router->add('GET', '/youdemy-V2/categories/cours/{id}', 'App\Controllers\CoursController', 'index');
+$router->add('GET', '/youdemy-V2/signup', 'App\Controllers\UserController', 'index');
 
 $router->dispatch();
